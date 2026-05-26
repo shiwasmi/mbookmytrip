d9d853991737933e29f6e17f82093fe1fbbf4cc1
shiwasmi <sagarchattar@gmail.com>
2026-05-26 10:33:32 +0530

    Repo publish

---
+++ src/main/java/com/example/mbookmytrip/MbookmytripApplication.java
@@ -0,0 +1,13 @@
+package com.example.mbookmytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbookmytripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbookmytripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mbookmytrip/MbookmytripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mbookmytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbookmytripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
